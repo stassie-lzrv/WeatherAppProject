23f3654192663f46530856b60ef4906a578f538c
stassie-lzrv <adlazareva@edu.hse.ru>
2022-11-30 00:18:18 +0300

    Initial Commit

---
+++ WeatherAppProject/WeatherAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppProjectApp.swift
+//  WeatherAppProject
+//
+//  Created by Настя Лазарева on 30.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
